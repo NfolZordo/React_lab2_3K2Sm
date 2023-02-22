0ef86549f1258a1bd0279b5d22230934a36974f1
John Doe <79935610+NfolZordo@users.noreply.github.com>
2023-02-22 18:59:43 +0200

    end created navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Container,
+  Form,
+  Button
+} from "react-bootstrap";
 
-function App() {
-  return (
-    <nav class="navbar navbar-expand-sm bg-light navbar-dark  bg-dark">
-    <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-            <img src="\logo.svg" style={{width:'40px'}}  alt="Avatar Logo"  className="rounded-pill"/>
-        </a>
-    </div>
+import logo from "./logo192.png";
 
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-    </nav>
-  );
-} 
-
-export default App;
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                height="30"
+                width="30"
+                className="d-inline-block align-top"
+                alt="Logo"
+              /> React Site
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" style={{justifyContent:'space-between'}}>
+              <Nav>
+                <Nav.Link href="/"> Home </Nav.Link>
+                <Nav.Link href="/about"> About us </Nav.Link>
+                <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                <Nav.Link href="/blog"> Blog </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="me-sm-3"
+                />
+                <Button variant="outline-info">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
+    )
+  }
+}
