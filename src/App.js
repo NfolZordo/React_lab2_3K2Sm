786ea6e03d40a7e3d11493a7647abbc9e370e844
John Doe <79935610+NfolZordo@users.noreply.github.com>
2023-02-22 18:32:02 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav class="navbar navbar-expand-sm bg-light navbar-dark  bg-dark">
+    <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+            <img src="\logo.svg" style={{width:'40px'}}  alt="Avatar Logo"  className="rounded-pill"/>
         </a>
-      </header>
     </div>
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
